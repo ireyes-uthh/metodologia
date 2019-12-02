a5a866c1a8cd1c1a8b28d620cd3760d6d4424200
Innego <innegonet@gmail.com>
2019-12-02 11:25:34 -0600

    2019-12-02

---
+++ Aplicacion3/Aplicacion3/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Innego
+ * Date: 26/11/2019
+ * Time: 10:31 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Aplicacion3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string nombres="";
+			string n1="";
+			string sexo="";
+			
+			Console.WriteLine("Ingresa nombre 1");
+			n1=Console.ReadLine();
+			Console.WriteLine("Sexo F/M");
+			sexo=Console.ReadLine();
+			nombres=nombres+"\n1.-"+n1+" sexo "+sexo ;
+			
+			Console.WriteLine("Ingresa nombre 2");
+			n1=Console.ReadLine();
+			Console.WriteLine("Sexo F/M");
+			sexo=Console.ReadLine();
+			nombres=nombres+"\n2.-"+n1+" sexo "+sexo ;
+
+			Console.WriteLine(nombres);
+
+			Console.ReadKey();
+		}
+
+	}
+}
